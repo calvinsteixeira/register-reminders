ecc43f3ce43ab428f79ac5dbdd9cd210ba1720dc
Calvin <calvin.teixeira@avmb-asten.com.br>
2024-03-01 15:04:34 -0300

    Adicionando componente de select box, form e lib zod

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 import AnimatedWrapper from "./animations/AnimatedWrapper";
+import SelectDateReminder from "./forms/SelectDateReminder"
 
-export { AnimatedWrapper }
+export { AnimatedWrapper, SelectDateReminder }

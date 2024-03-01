bfc69f343281dfb7095755335b60446036e480ff
Calvin <calvin.teixeira@avmb-asten.com.br>
2024-03-01 13:55:16 -0300

    Ajustando tela inicial da aplicação

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
+'use client'
+
+import React from "react"
+import { AnimatedWrapper } from "@/components/index"
+import { useRouter } from 'next/navigation'
+
+type Props = {}
+
 export default function Home() {
+
+  const router = useRouter()
+
+  // React.useEffect(() => {
+  //   setTimeout(() => { router.replace('/dashboard') }, 2000)
+  // }, [])
+
   return (
-    <main>
-      <div>Commit inicial</div>
+    <main className='w-screen h-screen bg-secondary flex items-center justify-center'>
+      <AnimatedWrapper
+        animationType="attentionSeeker"
+        attentionSeekerProps={{
+          delay: 200,
+          className: "flex",
+          effect: "tada",
+          triggerOnce: true
+        }}
+      >
+        <div className='flex flex-col gap-1 items-center justify-center'>
+          <h1 className='font-bold text-2xl text-secondary-foreground'>Register</h1>
+          <span className='font-bold text-2xl text-secondary-foreground'>Reminder</span>
+        </div>
+      </AnimatedWrapper>
     </main>
   )
 }

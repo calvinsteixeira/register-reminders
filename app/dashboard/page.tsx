e66e6f839a867bdc051a7d6527566adc2e4d0838
Calvin Teixeira <calvingsx@gmail.com>
2024-03-02 23:20:17 -0300

    Ajustando componente de datepicker para filtro dos lembretes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,31 +1,59 @@
-import React from 'react'
-import { DatePicker, Divider, ContentCard } from '@/components/index'
-import { Button } from "@/components/ui/button"
-import { CalendarIcon } from "lucide-react"
+"use client";
 
-type Props = {}
+import React from "react";
+import { DatePicker, Divider, ContentCard } from "@/components/index";
+import { Button } from "@/components/ui/button";
+import { CalendarIcon } from "lucide-react";
+import { FaSearch } from "react-icons/fa";
+import { Loader2 } from "lucide-react";
 
-export default function page({ }: Props) {
+type Props = {};
+
+export default function page({}: Props) {
+  const [loadingFilterRequest, setLoadingFiltarRequest] =
+    React.useState<boolean>(false);
+
   return (
-    <main className='w-screen h-screen background'>
-      <h1 className='text-lg font-bold text-secondary mt-12'>Register Reminder</h1>
-      <div className='w-full h-full mt-16 flex flex-col'>
-        <div className='space-y-4'>
-          <DatePicker label='Filtrar lembretes pela data' placeholder='selecione uma data' />
-          <Button size={'sm'} className='bg-secondary text-secondary-foreground'>Adicionar novo lembrete</Button>
+    <main className="w-screen h-screen background">
+      <h1 className="text-lg font-bold text-secondary mt-12">
+        Register Reminder
+      </h1>
+      <div className="w-full h-full mt-16 flex flex-col">
+        <div className="space-y-4">
+          <span className="text-sm text-secondary">
+            Selecione uma data para filtrar
+          </span>
+          <div className="flex gap-2">
+            <DatePicker />
+            <Button
+              disabled={loadingFilterRequest}
+              className="bg-secondary"
+              size={"icon"}
+            >
+              {loadingFilterRequest ? (
+                <Loader2 className="h-4 w-4 animate-spin" />
+              ) : (
+                <FaSearch />
+              )}
+            </Button>
+          </div>
           <Divider />
         </div>
-        <div className='flex flex-col gap-4 mt-4'>
-          <div className='flex flex-col gap-0.5 text-secondary'>
+        <div className="flex flex-col gap-4 mt-4">
+          <div className="flex flex-col gap-0.5 text-secondary">
             <h2>Meus lembretes</h2>
-            <div className='flex items-baseline gap-1'>
-              <CalendarIcon className='h-[0.85rem] w-[0.85rem]'/>
-              <p className='text-xs'>31/01/2024</p>
+            <div className="flex items-baseline gap-1">
+              <CalendarIcon className="h-[0.85rem] w-[0.85rem]" />
+              <p className="text-xs">31/01/2024</p>
             </div>
           </div>
-          <ContentCard title='Faculdade' subtitle='Documentos pendentes' description='Pedir para a escola os documentos necessário para efetivar minha matrícula' />
+          <ContentCard
+            title="Faculdade"
+            subtitle="Documentos pendentes"
+            description="Pedir para a escola os documentos necessário para efetivar minha matrícula"
+          />
         </div>
       </div>
     </main>
-  )
+  );
 }
--- components/inputs/DatePicker.tsx
+++ components/inputs/DatePicker.tsx
@@ -1,101 +1,55 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { format } from "date-fns"
-import { CalendarIcon } from "lucide-react"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { ptBR } from "date-fns/locale"
-import React from "react"
+import React from "react";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { ptBR } from "date-fns/locale";
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
-
-const FormSchema = z.object({
-  dob: z.date({
-    required_error: "É necessário informar uma data.",
-  }),
-})
+} from "@/components/ui/popover";
 
 type Props = {
-  label?: string,
-  placeholder?: string
-}
+  label?: string;
+};
 
-export default function DatePicker({label, placeholder}: Props) {  
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-  })
-
-  function onSubmit(data: z.infer<typeof FormSchema>) {
-    // Adicionar instruções
-  }
+export default function DatePicker({ label }: Props) {
+  const [date, setDate] = React.useState<Date>();
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-        <FormField
-          control={form.control}
-          name="dob"
-          render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <FormLabel className="mb-2">{label}</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant={"outline"}
-                      className={cn(
-                        "w-[240px] pl-3 text-left font-normal",
-                        !field.value && "text-muted-foreground"
-                      )}
-                    >
-                      {field.value ? (
-                        format(field.value, "PPP", { locale: ptBR })
-                      ) : (
-                        <span>{placeholder || "Selecione uma data"}</span>
-                      )}
-                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                  <Calendar
-                    mode="single"
-                    selected={field.value}
-                    onSelect={field.onChange}
-                    locale={ptBR}
-                    disabled={(date) =>
-                      date > new Date() || date < new Date("2024-01-01")
-                    }
-                    initialFocus
-                  />
-                </PopoverContent>
-              </Popover>
-              {/* <FormDescription>
-                Your date of birth is used to calculate your age.
-              </FormDescription> */}
-              <FormMessage />
-            </FormItem>
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[210px] justify-start text-left font-normal",
+            !date && "text-muted-foreground"
           )}
+        >
+          <>
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date ? (
+              format(date, "dd/MM/yyyy", { locale: ptBR })
+            ) : (
+              <span>{label || "Selecione uma data"}</span>
+            )}
+          </>
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          locale={ptBR}
+          onSelect={setDate}
+          initialFocus
         />
-        {/* <Button className='bg-secondary text-secondary-foreground' type="submit">Buscar</Button> */}
-      </form>
-    </Form>
-  )
+      </PopoverContent>
+    </Popover>
+  );
 }

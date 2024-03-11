989303abf40da3ce78a243714ead1c40fd54e754
Calvin <calvingsx@gmail.com>
2024-03-11 20:12:55 -0300

    Impossibilitando scroll durante o carregamento de requests com o uso de loader screen

--- components/content/LoaderScreen.tsx
+++ components/content/LoaderScreen.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import { Loader2 } from 'lucide-react'
+import React from "react";
+import { Loader2 } from "lucide-react";
 
 type Props = {
-  show: boolean
-  label?: string
-}
+  show: boolean;
+  label?: string;
+};
 
 export default function LoaderScreen({ show, label }: Props) {
+  React.useEffect(() => {
+    if (show) {
+      document.documentElement.classList.add("overflow-hidden");
+    } else {
+      document.documentElement.classList.remove("overflow-hidden");
+    }
+  }, [show]);
+
   return (
-    <div className={`z-10 absolute gap-4 ${show ? 'flex flex-row' : 'hidden'}  w-screen h-screen bg-black opacity-65 items-center justify-center m-0 p-0`}>     
-      <p className='text-white'>{label || 'Carregando'}...</p>
-      <Loader2 color='white' className="h-5 w-5 animate-spin" />
+    <div
+      className={`z-10 overflow-hidden absolute gap-4 ${
+        show ? "flex flex-row" : "hidden"
+      }  w-full h-full bg-black opacity-65 items-center justify-center m-0 p-0`}
+    >
+      <p className="text-white">{label || "Carregando"}...</p>
+      <Loader2 color="white" className="h-5 w-5 animate-spin" />
     </div>
-  )
+  );
 }
